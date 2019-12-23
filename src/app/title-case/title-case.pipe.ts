2ad1a10bba860ef1506a1db549a04755621eefc5
Baluditor <farkas.krisztian.balazs@gmail.com>
2019-12-23 23:22:25 +0100

    1 file modified

--- src/app/title-case/title-case.pipe.ts
+++ src/app/title-case/title-case.pipe.ts
@@ -10,12 +10,7 @@ export class TitleCasePipe implements PipeTransform {
 
     let words = value.split(' ');
     for (var i = 0; i < words.length; i++){
-      if(words[i] == 't'){
-        words[i] = words[i].substr(0,1).toUpperCase()+words[i].substr(1).toLocaleLowerCase();
-      } else {
-        words[i] = words[i].toLowerCase();
+      console.log(words[i]);
       }
     }
-  }
-
 }
