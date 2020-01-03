d9c3409c95d79649a74dacc4a5534697d69b8d0e
Baluditor <farkas.krisztian.balazs@gmail.com>
2020-01-03 11:33:20 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,30 @@ export class AppComponent {
     liked: false
   }
 
-  courses = [1,2,3]  
+  courses = [1,2,3]
+
+  animals = [1,2,3,4]
 
   onFavoriteChange(eventArgs: FavoriteChangeEventArgs){
     console.log('Favorite changed', eventArgs);
   }
+
+  viewMode = "something else";
+
+  forCourses = [
+    {id: 1, name: 'Course 1'},
+    {id: 2, name: 'Course 2'},
+    {id: 3, name: 'Course 3'},
+  ];
+
+  onAdd(){
+    this.forCourses.push({ id: 4, name: 'course4'});
+  }
+
+  onRemove(){
+    this.forCourses.pop()
+  }
+
+  
 
 }
