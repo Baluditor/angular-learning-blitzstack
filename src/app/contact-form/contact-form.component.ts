8d939891e6cbcfe196b517cfb61242d17b0e9720
Baluditor <farkas.krisztian.balazs@gmail.com>
2020-01-03 21:51:04 +0100

    2 files modified

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+    { id: 3, name: 'Mail' }
+  ]
+
+  radioContactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+    { id: 3, name: 'Mail' }
+  ]
+
   log(x) {console.log(x)}
   submit(f) {console.log(f)}
 
