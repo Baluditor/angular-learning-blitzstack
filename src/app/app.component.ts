9ed56555e73cf5200681bb43d1da2b84e0844aad
Baluditor <farkas.krisztian.balazs@gmail.com>
2020-01-03 12:17:47 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,9 +19,7 @@ export class AppComponent {
     liked: false
   }
 
-  courses = [1,2,3]
-
-  animals = [1,2,3,4]
+  courses = [1,2,3]  
 
   onFavoriteChange(eventArgs: FavoriteChangeEventArgs){
     console.log('Favorite changed', eventArgs);
@@ -35,6 +33,8 @@ export class AppComponent {
     {id: 3, name: 'Course 3'},
   ];
 
+  dogs = [1,2,3,4]
+
   onAdd(){
     this.forCourses.push({ id: 4, name: 'course4'});
   }
@@ -43,6 +43,27 @@ export class AppComponent {
     this.forCourses.pop()
   }
 
-  
+  onRemoveThis(course){
+    let index = this.forCourses.indexOf(course);
+    this.forCourses.splice(index, 1);
+  }
+
+  onChange(course){
+    course.name = "Changed";
+  }
+
+  animals;
+
+  loadAnimals(){
+    this.animals = [
+      {id: 1, name: 'Animal 1'},
+      {id: 2, name: 'Animal 2'},
+      {id: 3, name: 'Animal 3'},
+    ]; 
+  }
+
+  trackAnimal(index, animal){
+    return animal ? animal.id : undefined;
+  }
 
 }
